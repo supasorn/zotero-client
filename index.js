25e7b0b0ce9eae84c235a6489fc910130eaa3afb
Supasorn Suwajanakorn <supasorn@gmail.com>
2022-03-28 23:42:54 +0700

    auto commit

--- index.js
+++ index.js
@@ -8,13 +8,16 @@ const fs = require('fs');
 const glob = require("glob")
 const globp = require("glob-promise")
 
+let cred = JSON.parse(fs.readFileSync('zotero_credential.json'));
+console.log(cred.key, cred.userid);
+
 app.use(express.static("./"));
-app.use("/pdf_slim_viewer", express.static("/Users/supasorn/pdf_slim_viewer/"));
+app.use("/pdf_slim_viewer", express.static("pdf_slim_viewer/"));
 
 app.get('/sync', async (req, res) => {
   var config = new Zotero.RequestConfig();
-  config.Key("7ouvIbjpR1YFGTXxAFC3pGDa");
-  config.LibraryType('user').LibraryID('7902311');
+  config.Key(cred.key);
+  config.LibraryType('user').LibraryID(cred.userid);
   config.Target('items').Limit(100);
 
   
@@ -54,8 +57,8 @@ app.get('/sync', async (req, res) => {
 
 app.get('/fsync', async (req, res) => {
   var config = new Zotero.RequestConfig();
-  config.Key("7ouvIbjpR1YFGTXxAFC3pGDa");
-  config.LibraryType('user').LibraryID('7902311');
+  config.Key(cred.key);
+  config.LibraryType('user').LibraryID(cred.userid);
   config.Target('items').Limit(100);
 
   var fetcher = new Zotero.Fetcher(config.config);
@@ -72,8 +75,8 @@ app.get('/fsync', async (req, res) => {
 
 app.get('/delete', async (req, res) => {
   var config = new Zotero.RequestConfig();
-  config.Key("7ouvIbjpR1YFGTXxAFC3pGDa");
-  config.LibraryType('user').LibraryID('7902311');
+  config.Key(cred.key);
+  config.LibraryType('user').LibraryID(cred.userid);
   //config.Target('items').TargetModifier("top").Limit(100);
   config.Target('deleted');
   config.config["since"] = 1000;
@@ -101,7 +104,7 @@ app.get('/fetch_preview/:id', async (req, res) => {
 
   } catch(e) { // Preview doesn't exist
     console.log("not exist");
-    const files = await globp(`/Users/supasorn/Zotero/storage/${id}/*.pdf`);
+    const files = await globp(`${cred.path}/${id}/*.pdf`);
     if (files.length == 0)
       return res.sendStatus(404);
 
@@ -116,7 +119,7 @@ app.get('/fetch_preview/:id', async (req, res) => {
 app.get('/papers/:id/:name?', async (req, res) => {
   const id = req.params.id;
   //console.log(globp);
-  const files = await globp.promise(`/Users/supasorn/Zotero/storage/${id}/*.pdf`);
+  const files = await globp.promise(`${cred.path}${id}/*.pdf`);
   if (files.length == 0)
     return res.sendStatus(404);
   try {
@@ -142,81 +145,12 @@ app.get('/test', (req, res) => {
   res.end();
 });
 
+app.get('/arxiv', (req, res) => {
+  const query = req.params.query;
+  console.log(query)
+  res.write("yes");
+  res.end();
+});
+
+
 app.listen(port);
-
-/*
-fetcher.next().then((response)=>{
-	//the parsed json array of items that the api returned is on response.data,
-	//we can loop through them and create a Zotero.Item for each
-	console.log('\naccessing the items directly from this response:')
-	response.data.forEach((itemJson)=>{
-		let item = new Zotero.Item(itemJson);
-		console.log(item.get('title'));
-	});
-}).then(()=>{
-	//the array was also saved in fetcher.results
-	console.log('\nfetcher.results:');
-	fetcher.results.forEach((itemJson)=>{
-		let item = new Zotero.Item(itemJson);
-		console.log(item.get('title'));
-	});
-}).then(()=>{
-	if(fetcher.hasMore){
-		console.log('\nfetcher still has more results we can retrieve with a next()');
-	} else {
-		console.log('\nwe got all the results there are');
-	}
-
-	//totalResults is populated when the first set is retrieved, so we can tell how many more there are
-	console.log(`\nthere are ${fetcher.totalResults} results available, and we've already gotten ${fetcher.results.length}`);
-});*/
-
-
- /* 
-var config = new Zotero.RequestConfig().LibraryType('user').LibraryID(3).Target('publications').config;
-var fetcher = new Zotero.Fetcher(config);
-
-fetcher.next().then((response)=>{
-	//the parsed json array of items that the api returned is on response.data,
-	//we can loop through them and create a Zotero.Item for each
-	console.log('\naccessing the items directly from this response:')
-	response.data.forEach((itemJson)=>{
-		let item = new Zotero.Item(itemJson);
-		console.log(item.get('title'));
-	});
-}).then(()=>{
-	//the array was also saved in fetcher.results
-	console.log('\nfetcher.results:');
-	fetcher.results.forEach((itemJson)=>{
-		let item = new Zotero.Item(itemJson);
-		console.log(item.get('title'));
-	});
-}).then(()=>{
-	if(fetcher.hasMore){
-		console.log('\nfetcher still has more results we can retrieve with a next()');
-	} else {
-		console.log('\nwe got all the results there are');
-	}
-
-	//totalResults is populated when the first set is retrieved, so we can tell how many more there are
-	console.log(`\nthere are ${fetcher.totalResults} results available, and we've already gotten ${fetcher.results.length}`);
-});*/
-
-
-
-//import api from 'zotero-api-client';
-//const {default: api} = require('zotero-api-client');
-//console.log(api);
-//const myapi = api('7ouvIbjpR1YFGTXxAFC3pGDa').library('user', '7902311');
-//const itemsResponse = myapi.items().get();
-//itemsResponse.then((res) => {
-  //console.log(res);
-//});
-//console.log(itemsResponse);
-
-//var zotero = require('zotero');
-//var lib = zotero({ user: '7902311' });
-//console.log(lib);
-//console.log(lib.items());
-//var stream = new zotero.Stream({ apiKey: '7ouvIbjpR1YFGTXxAFC3pGDa' });
-//7ouvIbjpR1YFGTXxAFC3pGDa
